775333dc1d173dfcd9293b9993c7f208d4c34610
Drebec <drew.beck33@gmail.com>
2015-03-30 08:57:59 -0800

    First commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,43 @@
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author yaw
+ */
+public class BinaryTree<E extends Comparable> {
+
+    Node<E> root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(E newValue) {
+        if (root == null) {
+            root = new Node(newValue);
+        } else {
+            Node currentNode = root;
+            boolean placed = false;
+            while (!placed) {
+                if (newValue.compareTo(currentNode.getValue()) < 0) {
+                    if (currentNode.getLeft() == null) {
+                        currentNode.setLeft(new Node(newValue));
+                        currentNode.getLeft().setParent(currentNode);
+                        placed = true;
+                    } else {
+                        currentNode = currentNode.getLeft();
+                    }
+                } else {
+                    if (currentNode.getRight() == null) {
+                        currentNode.setRight(new Node(newValue));
+                        currentNode.getRight().setParent(currentNode);
+                        placed = true;
+                    } else {
+                        currentNode = currentNode.getRight();
+                    }
+                }
+            }
+        }
+    }
+}
